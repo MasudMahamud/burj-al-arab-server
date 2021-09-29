07d64613a745671b89d02d4e45fc5548f165a774
MasudMahamud <mahamud.al.msdud@gmail.com>
2021-09-30 01:29:08 +0600

    server-site

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const admin = require("firebase-admin");
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n50q4.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
+const port = 5000;
+
+const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const serviceAccount = require("./configs/burj-al-arab-sim-firebase-adminsdk-bnmfb-b8c86ae227.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const bookings = client.db("burjAlArab").collection("bookings");
+
+  app.post('/addBooking', (req, res) => {
+    const newBooking = req.body;
+    bookings.insertOne(newBooking)
+      .then(result => {
+        res.send(result.insertCount > 0);
+      })
+  })
+
+  app.get('/bookings', (req, res) => {
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith('Bearer ')) {
+      const idToken = bearer.split(' ')[1];
+      // idToken comes from the client
+      admin.auth().verifyIdToken(idToken)
+        .then((decodedToken) => {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          if (tokenEmail == queryEmail) {
+            bookings.find({ email: queryEmail })
+              .toArray((err, documents) => {
+                res.status(200).send(documents);
+              })
+          }
+          else {
+            res.status(401).send('un-authorized access');
+          }
+        })
+        .catch((error) => {
+          res.status(401).send('un-authorized access');
+        });
+    }
+    else {
+      res.status(401).send('un-authorized access');
+    }
+  })
+});
+
+
+app.listen(port)
